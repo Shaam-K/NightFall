efcf8b29c7d93ac59b15950f0b0e936341970463
Shaam <arkshaam@gmail.com>
2021-08-06 12:11:37 +0530

    Started creating form with search bar , added seperate css file to style it

--- weather_project/weather_app/static/js/nav.js
+++ weather_project/weather_app/static/js/nav.js
@@ -1,9 +1,5 @@
-/* Open */
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-/* Close */
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+function submit_form(){
+  var form = document.getElementById("my_form");
+  form.submit();
+  alert("Your Message Sent");
 }
