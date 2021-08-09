2343e12bf828c6356248645bb7c21af928694f00
Shaam <arkshaam@gmail.com>
2021-08-09 22:14:47 +0530

    got pollution api from openweather working, bugs need to fixed regarding rendering pollution_datas

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.shortcuts import render
 import urllib.request
 import json
@@ -5,25 +6,29 @@ import json
 def homepage(request):
     if request.method == 'GET':
         city = request.GET['location']
-
   # source contain JSON data from API
   
-        source = urllib.request.urlopen(
-            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=46868eed279cfe0e7919f07a4b87f369').read()
-  
+        source_weather = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=46868eed279cfe0e7919f07a4b87f369').read() 
         # converting JSON data to a dictionary
-        list_of_data = json.loads(source)
+        weather_load = json.loads(source_weather)
   
         # data for variable list_of_data
-        data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ' '
-                        + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + 'k',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
+        weather_data = {
+            "country_code": str(weather_load['sys']['country']),
+            "longitude": str(weather_load['coord']['lon']),
+            "latitude": str(weather_load['coord']['lat']),
+            "temp": str(weather_load['main']['temp']) + 'k',
+            "pressure": str(weather_load['main']['pressure']),
+            "humidity": str(weather_load['main']['humidity']),
         }
-        print(data)
+        print(weather_data)
+        longitude = weather_data['longitude']
+        latitude = weather_data["latitude"]
+        source_pollution = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/air_pollution?lat='+latitude+'&lon='+longitude+'&appid=0be1e7839601e89aa902e6a97e0ccbb5').read()
+        pollution_load = json.loads(source_pollution)
+
+        print(pollution_load)
     else:
-        data ={}
-    return render(request, "Home.html", data)
+        weather_data = {}
+
+    return render(request, "Home.html", weather_data)
