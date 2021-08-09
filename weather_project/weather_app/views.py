43014c359c4129208167ef8152ed44ce4417f514
Shaam <arkshaam@gmail.com>
2021-08-09 13:47:23 +0530

    form is now connected to openweather api, homepage is the function used

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-
+import urllib.request
+import json
 # Create your views here.
-def homepage(request, *args, **kwargs):
-    return render(request, "home.html", {})
+def homepage(request):
+    if request.method == 'GET':
+        city = request.GET['location']
+
+  # source contain JSON data from API
+  
+        source = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=46868eed279cfe0e7919f07a4b87f369').read()
+  
+        # converting JSON data to a dictionary
+        list_of_data = json.loads(source)
+  
+        # data for variable list_of_data
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data ={}
+    return render(request, "Home.html", data)
